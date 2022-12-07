f69f5c71c516185fdc38ec7aee5a0cc19afb0c82
ErikUTA <erik4tohj@gmail.com>
2022-12-07 04:11:58 -0600

    feat: new changes 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const canvasWidth = canvas.width = 600;
 const canvasHeight = canvas.height = 600;
-const animationDescription = document.getElementById('animationDescription');
+const animationDescription = document.getElementById('animationInfo');
 
 const playerImage = new Image();
 playerImage.src = './shadow_dog.png';
